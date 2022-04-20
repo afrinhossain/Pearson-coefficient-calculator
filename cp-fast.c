73ab06caf8cc5947027989d8d2aa96d8d511f9ed
Afrin Hossain <66248395+afrinhossain@users.noreply.github.com>
2022-04-20 22:02:59 +0300

    Faster calculator
    Uses known math properties to implement using sequential programming

---
+++ cp-fast.c
@@ -0,0 +1,55 @@
+/*
+This is the function you need to implement. Quick reference:
+- input rows: 0 <= y < ny
+- input columns: 0 <= x < nx
+- element at row y and column x is stored in data[x + y*nx]
+- correlation between rows i and row j has to be stored in result[i + j*ny]
+- only parts with 0 <= j <= i < ny need to be filled
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <vector>
+
+void correlate(int ny, int nx, const float *data, float *result) {
+    std::vector<double> tempmatrix(nx * ny);
+    double avg,sumofsquares ; 
+    
+    for(int row = 0; row < ny; row++){
+        avg = 0;
+        sumofsquares = 0;
+        for(int col = 0; col < nx; col++){
+            avg = avg + double(data[col + row*nx]);
+        }
+        avg = avg/nx;
+
+        for(int col = 0; col < nx; col++){
+            tempmatrix[col + row*nx] = data[col + row*nx]-avg;
+            sumofsquares = sumofsquares +double(tempmatrix[col + row*nx]*tempmatrix[col + row*nx]);
+        }
+        
+        //normalize the input rows so that each row has the arithmetic mean of 0
+        for(int col = 0; col < nx; col++){
+            tempmatrix[col + row*nx] = tempmatrix[col + row*nx]/sqrt(sumofsquares);
+        }
+        
+
+    }
+    double x; 
+    for(int row = 0; row < ny; row++){
+        for(int col = 0; col < ny; col++){
+            if(row <= col){
+                x= 0;
+                for(int k = 0; k < nx; k++){
+                    x =x +tempmatrix[k + row*nx]*tempmatrix[k + col*nx]; 
+                    
+                result[col + row*ny] = float(x); 
+                }
+            }
+        }
+    }
+    
+   
+
+}
